397373e32147dc55ab8f6eff27b00bd025bee73e
Mphiliseni <Mphilisenialexmhlongo@gmail.com>
2025-05-07 15:35:31 +0200

    Update assembly informational version and source link for Contoso Pizza

--- obj/Debug/net9.0/Contoso-Pizza.AssemblyInfo.cs
+++ obj/Debug/net9.0/Contoso-Pizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contoso-Pizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36fcd1f384b18df8aeab5cf0c1e1cc484396ed80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18fcfa1834a97ac6d1d191f3516bb4d98233502c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contoso-Pizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contoso-Pizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
